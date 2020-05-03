360fc2ccf30b70665e800b3f87ab37e49791d3cc
kecy <kec.bigmt@gmail.com>
2020-05-03 11:38:53 +0900

    add FeaturedTextList component

---
+++ src/components/FeaturedTextList.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+/**
+ * FeaturedTextList
+ * @param {Object} props
+ * @param {Array.<text: string>} props.items
+ */
+const FeaturedTextList = ({ items = [] }) => (
+  <ul className="featured-text-list">
+    {
+      items.map(item => (
+        <li key="item.text">{item.text}</li>
+      ))
+    }
+  </ul>
+)
+
+export default FeaturedTextList
